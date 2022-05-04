24e64407091e9fb1e675b0222c98f79e67db0de2
afeiship <1290657123@qq.com>
2022-05-05 06:56:43 +0800

    feat: upt tmpl to local

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,78 +1,25 @@
 'use strict';
 const Generator = require('yeoman-generator');
-const chalk = require('chalk');
 const yosay = require('yosay');
-const glob = require('glob');
-const { resolve } = require('path');
-const remote = require('yeoman-remote');
+const globby = require('globby');
 const yoHelper = require('@jswork/yeoman-generator-helper');
-const replace = require('replace-in-file');
 
-require('@jswork/next-registry-choices');
+const genp = require('@jswork/generator-prompts');
+const prompts = genp(['scope', 'registry', 'project_name', 'description']);
 
 module.exports = class extends Generator {
-  prompting() {
+  async prompting() {
     // Have Yeoman greet the user.
-    this.log(
-      yosay(`Welcome to the stunning ${chalk.red('generator-gulp')} generator!`)
-    );
-
-    const prompts = [
-      {
-        type: 'scope',
-        name: 'scope',
-        message: 'Your scope (eg: @babel )?',
-        default: 'jswork'
-      },
-      {
-        type: 'list',
-        name: 'registry',
-        message: 'Your registry',
-        choices: nx.RegistryChoices.gets()
-      },
-      {
-        type: 'input',
-        name: 'project_name',
-        message: 'Your project_name (eg: like this `react-button` )?',
-        default: yoHelper.discoverRoot
-      },
-      {
-        type: 'input',
-        name: 'description',
-        message: 'Your description?'
-      }
-    ];
-
-    return this.prompt(prompts).then((props) => {
-      this.props = props;
-      yoHelper.rewriteProps(props);
-    });
+    this.log(yosay(`Welcome to the stunning 'generator-gulp' generator!`));
+    this.props = await this.prompt(prompts);
   }
 
   writing() {
-    const done = this.async();
-    remote('afeiship', 'boilerplate-gulp', (_, cachePath) => {
-      this.fs.copyTpl(
-        glob.sync(resolve(cachePath, '{**,.*}')),
-        this.destinationPath(),
-        this.props
-      );
-      done();
-    });
-  }
-
-  end() {
-    const { project_name, description, scope } = this.props;
-    const files = glob.sync(resolve(this.destinationPath(), '{**,.*}'));
-
-    replace.sync({
-      files,
-      from: [
-        /boilerplate-gulp-description/g,
-        /boilerplate-gulp/g,
-        /boilerplate-scope/g
-      ],
-      to: [description, project_name, scope]
-    });
+    const ctx = yoHelper.ctx;
+    this.fs.copyTpl(
+      globby.sync(this.templatePath('**'), { dot: true }),
+      this.destinationPath(),
+      { ...this.props, ctx }
+    );
   }
 };
